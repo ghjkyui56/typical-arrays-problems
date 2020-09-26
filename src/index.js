c7fb03d67ec18aabdc92ae03ad17e1f4a68d23b9
ghjkyui56 <ksushashaborkina@mail.ru>
2020-09-26 23:34:40 +0300

    feat: make task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === 'undefined'){
+    return 0;
+  } else if (array.length < 1){
+    return 0;
+  }
+  let t = array[0];
+  for(let i = 1; i<array.length;i++){
+    if (array[i]<t){
+      t = array[i];
+    }
+  }
+  return t;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === 'undefined'){
+    return 0;
+  } else if (array.length < 1){
+    return 0;
+  }
+  if (array.length === 0){
+    return 0;
+  }
+  let t = 0;
+  for(let i = 0; i<array.length;i++){
+    if (array[i]>t){
+      t = array[i];
+    }
+  }
+  return t;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === 'undefined'){
+    return 0;
+  } else if (array.length < 1){
+    return 0;
+  }
+  let t = 0;
+  for(let i = 0; i<array.length;i++){
+    t = t + array[i];
+  }
+  t = t / (array.length);
+  return t;
 }
